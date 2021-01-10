e92378187d5956f0299616b1d771bc4d2a63a2f0
Abdelrahman Reda <boodycat09@gmail.com>
2021-01-10 23:11:37 +0200

    add WEB-INF

---
+++ build/generated/src/org/apache/jsp/login_jsp.java
@@ -0,0 +1,132 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class login_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  static {
+    _jspx_dependants = new java.util.ArrayList<String>(1);
+    _jspx_dependants.add("/layout/wrong_logging_credentials.jsp");
+  }
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <title>TODO supply a title</title>\n");
+      out.write("        <meta charset=\"UTF-8\">\n");
+      out.write("        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n");
+      out.write("        <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1\" crossorigin=\"anonymous\">\n");
+      out.write("        <link rel=\"stylesheet\" href=\"style2.css\">\n");
+      out.write("        <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW\" crossorigin=\"anonymous\"></script>\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        <div class=\"container\">\n");
+      out.write("            <div class=\"row\" >\n");
+      out.write("                <div class=\"col-lg-12 \" style=\"padding-top: 225px\">\n");
+      out.write("                    <form  class=\"login-form\" method=\"POST\" action=\"validate\" >\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("                        ");
+  response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+      out.write("\n");
+      out.write("                        ");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("\n");
+
+    if (session.getAttribute("wrong_credentials") != null) {
+
+      out.write("\n");
+      out.write("<div class=\"alert alert-danger\" role=\"alert\">\n");
+      out.write("    This is a danger alert—check it out!\n");
+      out.write("</div>                            \n");
+}
+    session.removeAttribute("wrong_credentials");
+
+      out.write('\n');
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("                        <div class=\"mb-3\">\n");
+      out.write("                            <label for=\"customer_id\" class=\"form-label\">Student Mail</label>\n");
+      out.write("                            <input type=\"mail\" class=\"form-control  \" id=\"customer_id\" name=\"email\">\n");
+      out.write("\n");
+      out.write("                        </div>\n");
+      out.write("                        <div class=\"mb-3\">\n");
+      out.write("                            <label for=\"customer_password\" class=\"form-label\">Password</label>\n");
+      out.write("                            <input type=\"password\" class=\"form-control \" id=\"customer_password\" name=\"password\">\n");
+      out.write("\n");
+      out.write("                        </div>\n");
+      out.write("                        <input type=\"hidden\"  name=\"user_type\" value=\"student\">\n");
+      out.write("\n");
+      out.write("                        <button style=\"width: 100%\" type=\"submit\" class=\"btn btn-primary\">Sign in</button>\n");
+      out.write("                        <div class=\"d-flex justify-content-center align-items-center mt-2 flex-column\">\n");
+      out.write("                            <small text-center>Are you Staff Member?</small>\n");
+      out.write("                            <br>\n");
+      out.write("                            <a style=\"text-decoration: underline\" href=\"staffLogin.jsp\">Login in as Staff Member</a>\n");
+      out.write("\n");
+      out.write("                        </div>\n");
+      out.write("                    </form>    \n");
+      out.write("                </div>\n");
+      out.write("            </div>\n");
+      out.write("        </div>\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
---
+++ build/web/staff_members.jsp
@@ -0,0 +1,81 @@
+<%-- 
+    Document   : staff_members
+    Created on : Jan 9, 2021, 12:43:11 AM
+    Author     : boody
+--%>
+
+<%@page import="java.util.ArrayList"%>
+<%@page import="Models.Staff"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<body>
+    <%@include file="layout/header.jsp" %> 
+    <%  response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");%>
+
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <%@ include file = "layout\sidebar.jsp" %>
+        <!-- Page Content  -->
+        <div id="content">
+            <%@ include file = "layout\navbar.jsp" %>
+            <div class="card" >
+                <div class="card-header">
+                    <h3>Find Staff Member</h3>
+                </div>
+                <div class="card-body">
+
+                    <table id="example" class="table table-striped table-bordered" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Staff Name</th>
+                                <th>Department</th>
+                                <th>Mail</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <%
+                                if (request.getAttribute("staffMembers") != null) {
+
+                                    ArrayList<Staff> list = (ArrayList<Staff>) request.getAttribute("staffMembers");
+                                    for (int i = 0; i < list.size(); i++) {
+                                        out.print("<tr><td>" + (i + 1) + "</td>");
+                                        out.print("<td><a  style=\"text-decoration:underline\" href = /Office_Hours/OfficeHoursController?getStaffMembers=" + list.get(i).id +" >" + list.get(i).user_name + "</a></td>");
+                                        out.print("<td>" + list.get(i).dapartment + "</td>");
+                                        out.print("<td><a style=\"text-decoration:underline\" href = \"\" >" + list.get(i).mail + "</td></tr>");
+                                    }
+                                }
+
+                            %>
+
+                        </tbody>
+                    </table>
+
+
+
+
+                   
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+<%@ include file = "layout\scripts.jsp" %>
+<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js" ></script>
+<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js" ></script>
+<script>
+    $(document).ready(function () {
+        $('#example').DataTable();
+    });
+
+</script>
+<script>  $('.staff-member').toggleClass('activeElement');</script>
